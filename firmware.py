5cafa14c35d8496faf3b58482944cd051ff0933d
Vinh Vo <105337753+vvinhvvo98@users.noreply.github.com>
2024-01-06 19:54:37 -0800

    Update

--- firmware.py
+++ firmware.py
@@ -2,26 +2,34 @@
     @section sec_o      Overview 
                         asdas
                         
+                        
     @section sec_1      Custom PCB
                         @image html pcb.png
+                        
                         
     @section sec_2      Arduino Nano
                         @image html mcu.png
                         
+                        
     @section sec_3      Step Down Regulator
                         @image html stepdown.png
+                        
                         
     @section sec_4      NEMA 17 Stepper Motor 
                         @image html nema.png
                         
+                        
     @section sec_5      OLED Screen
                         @image html oled.png
+                        
                         
     @section sec_6      Keyboard 4x4
                         @image html keyboard.png
                         
+                        
     @section sec_7      RGB LED      
                         @image html led.png   
+                        
                         
    @author              Vinh Vo
    @date                Sep 12, 2023
